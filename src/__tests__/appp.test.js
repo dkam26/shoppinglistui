18322622a2e1934aaf8c38b449da578f536b9617
kamara deo <deo.kamara@andela.com>
2018-02-19 15:49:17 +0300

    [Chore #154561615]Read me

---
+++ src/__tests__/appp.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import {configure} from 'enzyme';
+import App  from '../App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import  toJson,{ shallowToJson } from 'enzyme-to-json';
+import LocalStorageMock from '../setupTests';
+import ReactDOM from 'react-dom';
+configure({ adapter: new Adapter() });
+describe('app component', () => {
+    const props={
+    
+        render:()=>{}
+     }
+    const wrapper =  shallow(<App  />);
+    it('should run', () => {
+        expect(wrapper.exists(<div></div>)).toBe(true);
+    });
+    it('should ve the following method',()=>{
+        wrapper.instance().render();
+       
+       
+       });
+})
