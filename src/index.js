d13431f8c44ab57296ab5cff7a3820ce2e8f8e1f
kamara deo <deo.kamara@andela.com>
2018-02-20 11:32:52 +0300

    [Chore #154561615]Read me

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
    
@@ -13,4 +13,4 @@ ReactDOM.render(
     
     ,document.getElementById('root')
 );
-registerServiceWorker();
+
